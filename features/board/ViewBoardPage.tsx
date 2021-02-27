f63aa6ca4cb30652107389e9ae4accc24d298a21
Ted Kesgar <t.kesgar@outlook.com>
2021-02-27 21:42:38 +0700

    Implement section

--- features/board/ViewBoardPage.tsx
+++ features/board/ViewBoardPage.tsx
@@ -1,37 +1,80 @@
 import DefaultLayout from "@/components/DefaultLayout";
-import { useBoard } from "@/services/firebase/board";
+import { useBoard, useBoardSections } from "@/services/firebase/board";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import * as React from "react";
-import { Button, Container, Spinner } from "react-bootstrap";
+import { Button, Col, Container, Row, Spinner } from "react-bootstrap";
 import { Settings } from "react-feather";
+import style from "./ViewBoardPage.module.scss";
+import classnames from "classnames";
+import MainNavbar from "@/components/MainNavbar";
 
 export default function ViewBoardPage(): JSX.Element {
   const router = useRouter();
   const board = useBoard(router.query.boardId as string);
+  const sections = useBoardSections(router.query.boardId as string);
 
   return (
-    <DefaultLayout>
-      <Container className="my-4">
-        {board ? (
-          <div className="border-bottom border-light pb-3 mb-4 d-flex">
-            <div className="flex-fill">
-              <h1>{board.title}</h1>
-              <div className="text-muted mb-3">{board.description}</div>
-            </div>
-            <div className="ml-3">
-              <Link href={`/${router.query.boardId}/settings`} passHref>
-                <Button variant="secondary">
-                  <Settings size="16" />
-                  <span className="sr-only">Settings</span>
-                </Button>
-              </Link>
+    <div className="min-vh-100 d-flex flex-column">
+      <MainNavbar />
+      {board && sections ? (
+        <>
+          <div>
+            <Container fluid className="my-3">
+              <div className="border-bottom border-light pb-3 d-flex">
+                <div className="flex-fill">
+                  <h1>{board.title}</h1>
+                  <div className="text-muted mb-3">{board.description}</div>
+                </div>
+                <div className="ml-3">
+                  <Link href={`/${router.query.boardId}/settings`} passHref>
+                    <Button variant="secondary">
+                      <Settings size="16" />
+                      <span className="sr-only">Settings</span>
+                    </Button>
+                  </Link>
+                </div>
+              </div>
+            </Container>
+          </div>
+          <div className={classnames(style.Viewport, "flex-fill")}>
+            <div
+              className={classnames(
+                style.Container,
+                style[`SectionCount${board.sectionCount}`],
+                "mx-auto my-3"
+              )}
+            >
+              <Container fluid>
+                <Row>
+                  {sections.map((section) => (
+                    <Col key={section.id}>
+                      <h2
+                        className={classnames(
+                          style.SectionTitle,
+                          "h5 text-center mb-3"
+                        )}
+                      >
+                        {section.title}
+                      </h2>
+                      <div className="bg-light mt-5 py-5">Hello world!</div>
+                      <div className="bg-light mt-5 py-5">Hello world!</div>
+                      <div className="bg-light mt-5 py-5">Hello world!</div>
+                      <div className="bg-light mt-5 py-5">Hello world!</div>
+                      <div className="bg-light mt-5 py-5">Hello world!</div>
+                    </Col>
+                  ))}
+                </Row>
+              </Container>
             </div>
           </div>
-        ) : (
-          <Spinner animation="border" className="d-block mx-auto my-5" />
-        )}
-      </Container>
-    </DefaultLayout>
+        </>
+      ) : (
+        <Spinner
+          animation="border"
+          className="d-block text-center mx-auto my-5"
+        />
+      )}
+    </div>
   );
 }
--- services/firebase/board.ts
+++ services/firebase/board.ts
@@ -7,6 +7,12 @@ export interface Board {
   id: string;
   title: string;
   description: string;
+  sectionCount: number;
+}
+
+export interface Section {
+  id: string;
+  title: string;
 }
 
 interface CreateBoardData {
@@ -41,27 +47,63 @@ export async function createBoard(
   return boardRef.id;
 }
 
-export function useBoard(id: string): Board {
+export function useBoard(boardId: string): Board {
   const [board, setBoard] = React.useState<Board>();
 
   React.useEffect(() => {
-    if (!id) {
+    if (!boardId) {
       return;
     }
 
     return db()
       .collection("boards")
-      .doc(id)
-      .onSnapshot((doc) => {
-        if (doc.exists) {
-          setBoard(doc.data() as Board);
+      .doc(boardId)
+      .onSnapshot((snapshot) => {
+        if (snapshot.exists) {
+          const data = snapshot.data();
+          setBoard({
+            id: snapshot.id,
+            title: data.title,
+            description: data.description,
+            sectionCount: data.sectionCount,
+          });
         } else {
           setBoard(null);
         }
       });
-  }, [id]);
+  }, [boardId]);
 
   return board;
+}
+
+export function useBoardSections(boardId: string): Section[] {
+  const [sections, setSections] = React.useState<Section[]>();
+
+  React.useEffect(() => {
+    if (!boardId) {
+      return;
+    }
+
+    return db()
+      .collection("boards")
+      .doc(boardId)
+      .collection("sections")
+      .onSnapshot((querySnapshot) => {
+        const newSections: typeof sections = [];
+
+        querySnapshot.forEach((result) => {
+          const data = result.data();
+          newSections.push({
+            id: result.id,
+            title: data.title,
+          });
+        });
+
+        setSections(newSections);
+      });
+  });
+
+  return sections;
 }
 
 export async function getMyBoards(uid: string): Promise<Board[]> {
@@ -78,6 +120,7 @@ export async function getMyBoards(uid: string): Promise<Board[]> {
       id: result.id,
       description: data.description,
       title: data.title,
+      sectionCount: data.sectionCount,
     });
   });
 
